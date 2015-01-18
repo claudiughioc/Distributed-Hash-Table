951b1189f12596ca326cc09d9818e0580c22088c
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2015-01-18 23:22:25 +0200

    Add distribution graph and gnuplot script
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

---
+++ results/distribution_plot.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/gnuplot 
+reset
+set title "Distribution of 1000 Objects"
+set terminal png
+set xlabel "Nr Objects"
+set ylabel "Server Index"
+set xrange[0:11]
+
+
+set boxwidth 0.5
+set style fill solid
+
+set output 'distribution.png'
+plot "distribution" using 1:2 with boxes lt rgb "#406090" title "Distribution"
